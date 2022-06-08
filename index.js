a4043795f442deb8e6556a6c31e55e02560cdf6f
derrique <otieno.derrick@student.moringaschool.com>
2022-06-08 19:11:17 +0300

    commit tree 401deb8b4e4fbd8f1e00eab6c242de267ae896e5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const companyName = "Scuber"
+print (companyName)
+
+
+let mostProfitableNeighborhood = "Chelsea"
+print (mostProfitableNeighborhood)
+
+let companyCeo = "Susan Smith"
+print (companyCeo)
+ 
